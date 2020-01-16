2bb4334fb857d9428efb80bf220cf1e4a6fb7d18
yektan <aliyektan.mutlu@kafein.com.tr>
2020-01-16 14:57:00 +0300

    project creation

---
+++ src/test/java/com/turkcell/crm/assetmanagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.crm.assetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
